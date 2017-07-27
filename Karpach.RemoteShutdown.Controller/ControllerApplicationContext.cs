e6d86b979e82ad71d39996ec305865940167a408
Viktar Karpach <vkarpach@gmail.com>
2017-07-26 23:37:25 -0500

    Initial commit

---
+++ Karpach.RemoteShutdown.Controller/ControllerApplicationContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Karpach.RemoteShutdown.Controller
+{
+    public class ControllerApplicationContext: ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public ControllerApplicationContext()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();            
+            var notifyContextMenu = new ContextMenuStrip();
+
+            var hibernate = new ToolStripMenuItem("Hibernate")
+            {
+                Image = Resources.Shutdown.ToBitmap()
+            };
+            hibernate.Click += Hibernate;
+            notifyContextMenu.Items.Add(hibernate);
+
+            notifyContextMenu.Items.Add("-");
+
+            var exit = new ToolStripMenuItem("Exit")
+            {
+                Image = Resources.Exit.ToBitmap()
+            };
+            exit.Click += Exit;
+            notifyContextMenu.Items.Add(exit);
+
+
+            // Initialize Tray Icon            
+            _trayIcon = new NotifyIcon
+            {
+                Icon = Resources.AppIcon,
+                ContextMenuStrip = notifyContextMenu,
+                Visible = true
+            };
+            Task.Run(() =>
+            {
+                var host = new WebHostBuilder()                                                            
+                    .UseUrls("http://+:5001")
+                    .UseKestrel()
+                    .Configure(app =>
+                    {
+                        app.Run(async context =>
+                        {
+                            Hibernate(this,null);    
+                            context.Response.StatusCode = 204;
+                            await Task.Yield();
+                        });
+                    })
+                    .Build();
+                host.Run();
+            }, _cancellationTokenSource.Token);
+        }
+
+        private void Hibernate(object sender, EventArgs e)
+        {
+            Application.SetSuspendState(PowerState.Hibernate, true, true);
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;            
+            _cancellationTokenSource.Cancel();
+            Application.Exit();
+        }        
+    }
+}
--- Karpach.RemoteShutdown.Controller/Program.cs
+++ Karpach.RemoteShutdown.Controller/Program.cs
@@ -16,7 +16,7 @@ namespace Karpach.RemoteShutdown.Controller
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ControllerApplicationContext());
         }
     }
 }
