a54807d342b48dec37b58853a8f0e008ec0116d1
Viktar Karpach <vkarpach@gmail.com>
2022-07-25 23:20:26 -0500

    Update Readme and AssemblyInfo

--- Karpach.RemoteShutdown.Controller/Properties/AssemblyInfo.cs
+++ Karpach.RemoteShutdown.Controller/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Karpach.RemoteShutdown.Controller")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
